a9f91419599114f9ea03a3aba01168901a8b3001
Rohan Gaikwad <109089439+RohanGaikwad70@users.noreply.github.com>
2022-08-16 13:27:50 +0530

    Create 2While.cpp

---
+++ Loops, switch case & functions/2While.cpp
@@ -0,0 +1,23 @@
+
+
+// SUM OF N EVEN NUMBERS USING WHILE LOOP
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,i=2,sum=0;
+    cout<<"enter n"<<endl;
+    cin>>n;
+
+    while(i<n)
+    {
+        sum=sum+i;
+        i=i+2;
+
+    }
+    cout<<"sum of even number from  2 to "<<n<<"is"<< sum<<endl;
+    return 0;
+    
+}
