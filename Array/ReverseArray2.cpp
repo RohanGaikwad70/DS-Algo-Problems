7ad7b2320874970b281841e0b545e73fa6dbc763
Rohan Gaikwad <109089439+RohanGaikwad70@users.noreply.github.com>
2022-10-31 10:19:12 -0700

    Create ReverseArray2.cpp

---
+++ Array/ReverseArray2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+
+void ulta(int arr[],int n)
+{
+    
+    for(int i=0;i<=;i++)
+    {
+        start=0;
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Arrays Ki Size batao bhai: ";
+    cin>>n;
+    int rg[n];
+
+
+cout<<" Arrays ke element enter karo bhai:  ";
+for(int i=0;i<=n;i++)
+{
+    cin>>rg[i];
+}
+cout<<"sidha Array :";
+for(int i=0;i<=n;i++)
+{
+    cout<<rg[i]<<" ";
+}
+// ulta array
+    ulta(rg,n);
+cout<<" ulta Array:";
+for(int i=0;i<=n;i++)
+{
+
+    cout<<rg[i]<<" ";
+
+}
+
+}
+
+
